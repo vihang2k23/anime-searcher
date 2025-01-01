9051d9e05f60b1430f6a816d6a617e107393b10e
Vihang Patel <vihangp@zignuts.com>
2025-01-01 19:15:40 +0530

    create the folder structure according to the project

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import vuetify from './plugins/vuetify';
+import router from './router';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
+app.mount('#app');
